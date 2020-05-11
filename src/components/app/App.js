ec6af6d2a7ddd712cc5179fa593cf3aed5d9d1b0
JashanB <jashan_brar50@hotmail.com>
2020-05-10 20:23:42 -0400

    added react router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <h1 className="yoga-logo">Yoga-Me</h1>
+      <div className="App">
+        <Switch>
+          <Route path='/users/:user_id/places/:id' render={(props) => <Place {...props} />} />
+          <Route path='/users/:id' render={(props) => <Search {...props} />} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
