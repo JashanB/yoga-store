9a2d56b58cdaa0be63ca8435c953496d03e4a614
JashanB <jashan_brar50@hotmail.com>
2020-05-28 18:23:26 -0400

    removed canva script, added background img and logo

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -19,19 +19,13 @@ function App() {
     //       <Route path='/users/:id' render={(props) => <Search {...props} />} />
     //     </Switch>
     //   </div> */}
-      
+
     // {/* </Router> */}
     // </div>
     <div className="App">
-    <div
-    className="canva-embed"
-    data-design-id="DAD9IlS-dB4"
-    data-height-ratio="1.7778"
-    style={divStyle}
-   ></div>
-   {/* <script async src="https:&#x2F;&#x2F;sdk.canva.com&#x2F;v1&#x2F;embed.js"></script> */}
-   <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAD9IlS-dB4&#x2F;view?utm_content=DAD9IlS-dB4&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener"></a>
-   </div>
+    <img className="logo-img" src="yogalogo.png"></img>
+    <img className="yoga-background-img"src="yoga.png"></img>
+    </div>
   );
 }
 
