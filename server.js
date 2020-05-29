52ca2b72c3514b06c77fdeef6e9cf4c354c76c55
JashanB <jashan_brar50@hotmail.com>
2020-05-29 19:42:18 -0400

    established db connection

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+// load .env data into process.env
+require('dotenv').config();
 
-const app = express();
-const port = process.env.PORT || 3000;
+// Web server config
+const PORT       = process.env.PORT || 3000;
+const ENV        = process.env.ENV || "development";
+const express    = require("express");
+const bodyParser = require("body-parser");
+const cookieSession = require('cookie-session');
+const sass       = require("node-sass-middleware");
+const app        = express();
+const morgan     = require('morgan');
+const methodOverride = require('method-override');
+const database   = require("./database");
 
-app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/styles", sass({
+  src: __dirname + "/styles",
+  dest: __dirname + "/public/styles",
+  debug: true,
+  outputStyle: 'expanded'
+}));
+app.use(express.static("public"));
+app.use(cookieSession({
+  name: 'session',
+  secret: 'midterm',
+  // Cookie Options
+  maxAge: 500 * 60 * 1000 // 5 minutes
+}));
+app.use(methodOverride('_method'));
+
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
