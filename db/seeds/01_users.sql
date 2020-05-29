0e60b4936c24abeb4ac785df3454518d4ca5867d
JashanB <jashan_brar50@hotmail.com>
2020-05-29 19:27:50 -0400

    added pool and model

--- db/seeds/01_users.sql
+++
@@ -1,6 +0,0 @@
-insert into users (email) values ('katherine@lighthouse.com');
-insert into users (email) values ('jamie@lighthouse.com');
-insert into users (email) values ('joshon@lighthouse.com');
-insert into users (email) values ('clara@lighthouse.com');
-insert into users (email) values ('susan@lighthouse.com');
-
--- db/seeds/02_topics.sql
+++
@@ -1,5 +0,0 @@
-insert into topics (name) values ('Biology');
-insert into topics (name) values ('Chemistry');
-insert into topics (name) values ('Programming');
-insert into topics (name) values ('Math');
-insert into topics (name) values ('Physics');
