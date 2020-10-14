5aeb30c4367869bd55a5f9286a5195ab3977a3cc
JashanB <jashan_brar50@hotmail.com>
2020-10-13 21:30:38 -0400

    resetting db- fix errors

--- db/schema/14_teacher_events.sql
+++ db/schema/14_teacher_events.sql
@@ -4,6 +4,5 @@ CREATE TABLE teacher_events (
   teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
   event_id INTEGER REFERENCES events(id) ON DELETE CASCADE,
   PRIMARY KEY (teacher_id, event_id),
-  teacher_sign_in BOOLEAN NOT NULL DEFAULT FALSE,
-
+  teacher_sign_in BOOLEAN NOT NULL DEFAULT FALSE
 );
--- db/seeds/01_locations.sql
+++ db/seeds/01_locations.sql
@@ -1,6 +1,6 @@
-insert into locations (name, address) values ('Richmond and Spadina');
-insert into locations (name, address) values ('Dundas and York');
-insert into locations (name, address) values ('Eaton Centre');
-insert into locations (name, address) values ('Mexico City');
+insert into locations (name, address) values ('Richmond and Spadina', '183 Spadina Street, Toronto ON, Canada');
+insert into locations (name, address) values ('Dundas and York', '95 Dundas Street, Toronto ON, Canada');
+insert into locations (name, address) values ('Eaton Centre', '1 Eaton Centre, Toronto ON, Canada');
+insert into locations (name, address) values ('Mexico City', '98 Road Street, Mexico City, Mexico');
 
 
