165de51eda36cf8d4c7e3b089bf9f24857687052
JashanB <jashan_brar50@hotmail.com>
2020-10-24 11:33:42 -0400

    adding class component

---
+++ client/src/components/class/Class.js
@@ -0,0 +1,9 @@
+import './Class.css'
+
+export default function Class(props) {
+
+  return (
+    <>
+    </>
+  )
+}
--- client/src/components/schedule/Schedule.js
+++ client/src/components/schedule/Schedule.js
@@ -3,8 +3,8 @@ import './Schedule.css'
 export default function Schedule(props) {
 
   return (
-    <div className="tile">
-      <button className={className} onClick={() => handleClick(col, row, props.status)}></button>
+    <div className="schedule">
+
     </div>
   )
 }
