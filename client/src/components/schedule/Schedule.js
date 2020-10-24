71aafa7d429853e4863a4f66affd79bc62d8daff
JashanB <jashan_brar50@hotmail.com>
2020-10-24 12:34:52 -0400

    adding insert class post function

--- client/src/components/schedule/Schedule.js
+++ client/src/components/schedule/Schedule.js
@@ -1,12 +1,19 @@
 import './Schedule.css'
 import Class from '../class'
 import React from 'react';
+import axios from 'axios';
 
 export default function Schedule(props) {
+  const reserveClass = function () {
+    axios.post(`http://localhost:3001/`, {
+      name: 'bob'
+    })
+  }
   const listOfClasses = props.classes.map(function(classObj, index) {
+    const id = classObj.id
     return (
       <Class
-        key={index}
+        key={id}
       />
     )
   })
