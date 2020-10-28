0a5a9cc3b848d530293ac27125ffa77658b965dd
JashanB <jashan_brar50@hotmail.com>
2020-10-27 21:34:02 -0400

    adding login session

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const session = require('express-session');
 
 module.exports = (db) => {
-  router.get("/:user_id", (req, res) => {
-    const id = req.session.user_id;
+  router.get("/", (req, res) => {
+    const sess = req.session;
+    const email = req.body.email
+    const password = req.body.password
+    // const id = req.session.user_id;
+    //if email and password present, redirect to home, else redirect to login
     if (id) {
       let userId = parseInt(req.session.user_id);
       if (id === userId) {
