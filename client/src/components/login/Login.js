5865ab89c90db5009275dd3ebffb931bfc0f224e
JashanB <jashan_brar50@hotmail.com>
2020-10-27 20:11:04 -0400

    adding login

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -3,11 +3,42 @@ import Class from '../class'
 import React from 'react';
 import axios from 'axios';
 
-export default function Login(props) {
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
 
   return (
-    <div className="login">
-
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <ControlLabel>Email</ControlLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+        </FormGroup>
+        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+          Login
+        </Button>
+      </form>
     </div>
-  )
+  );
 }
