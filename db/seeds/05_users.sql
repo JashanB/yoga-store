7f7d99c383e9a4f36c95382220748c0ff68b7417
JashanB <jashan_brar50@hotmail.com>
2020-11-05 20:46:52 -0500

    updated seeds to match model

--- db/seeds/05_users.sql
+++ db/seeds/05_users.sql
@@ -1,5 +1,5 @@
 insert into users (first_name, last_name, email, password, cc, address) values ('Sam', 's', 'a@a', 'xtx', '123', '7124 street');
-insert into users (first_name, last_name, email, password, cc, address) values ('Sara', 'ds', 'a@a', 'xtx', '123', '7124 street');
-insert into users (first_name, last_name, email, password, cc, address) values ('Rachel', 'asd', 'a@a', 'xtx', '123', '7124 street');
-insert into users (first_name, last_name, email, password, cc, address) values ('Uniqlo', 'bot', 'a@a', 'xtx', '123', '7124 street');
-insert into users (first_name, last_name, email, password, cc, address) values ('Erin', 'Hugesack', 'a@a', 'xtx', '123', '7124 street');
+insert into users (first_name, last_name, email, password, cc, address) values ('Sara', 'ds', 'b@a', 'xtx', '123', '7124 street');
+insert into users (first_name, last_name, email, password, cc, address) values ('Rachel', 'asd', 'c@a', 'xtx', '123', '7124 street');
+insert into users (first_name, last_name, email, password, cc, address) values ('Uniqlo', 'bot', 'd@a', 'xtx', '123', '7124 street');
+insert into users (first_name, last_name, email, password, cc, address) values ('Erin', 'Hugesack', 'e@a', 'xtx', '123', '7124 street');
